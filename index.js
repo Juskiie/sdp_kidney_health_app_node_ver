8493c8d989a53e90ef1e146a5a648ab8c81acae7
Juskiie <juskiehusky@icloud.com>
2023-02-15 20:40:00 +0000

    Updated back end code, now add's a test file to sql server on load for a user in users table. NOTES: - 'Users' table: will store user login information - 'patients' table: will store patient details

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+const mysql = require('mysql');
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    password: 'SDPKodeGreen123',
+    database: 'maindb'
+});
+
+const updateUsersTable = `
+    UPDATE users
+    SET email = ?, password = ?
+    WHERE id = ?;
+`
+const valuesForUsers = ['email@host.com','example_pwrd',1];
+
+pool.query(updateUsersTable, valuesForUsers, (error, results) => {
+    if (error) throw error;
+    console.log(`Updated ${results.affectedRows} row(s)`);
+})
+
+pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log('Connected to MySQL server!');
+    connection.release();
+});
+
+
 const express = require('express');
 const app = express();
 const port = 3000; // choose any port number you like
@@ -7,7 +35,7 @@ app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
 
-// Link to html
+// Link to html -- load main page
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
