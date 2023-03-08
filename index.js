5e7e05389d748ebc1455163cd32681336a9fc9ca
Juskiie <juskiehusky@icloud.com>
2023-03-08 15:52:54 +0000

    -minor changes

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ app.get('/', (req, res) => {
 // SQL QUERIES
 app.use(express.json());
 
+const createUsersTable = `
+CREATE TABLE \`users\` (
+  \`id\` int NOT NULL AUTO_INCREMENT,
+  \`name\` varchar(255) NOT NULL,
+  \`email\` varchar(255) NOT NULL,
+  \`password\` varchar(255) NOT NULL,
+  PRIMARY KEY (\`id\`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+`
+
 // Update the users table
 const updateUsersTable = `
     UPDATE users
