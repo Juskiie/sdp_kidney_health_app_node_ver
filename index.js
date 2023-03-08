fd73b7b3fbdaa2ef4e6f7f85a5fbeadbb7289f9a
Juskiie <juskiehusky@icloud.com>
2023-03-08 15:50:15 +0000

    -minor changes

--- index.js
+++ index.js
@@ -30,6 +30,19 @@ const updateUsersTable = `
     SET email = ?, password = ?
     WHERE id = ?;
 `
+
+const createPatientsTable = `
+CREATE TABLE \`patients\` (
+  \`ID\` int NOT NULL AUTO_INCREMENT COMMENT 'A unique ID for each patient in the database',
+  \`name\` varchar(255) DEFAULT NULL COMMENT 'The full name for the patient',
+  \`age\` int DEFAULT NULL COMMENT 'Stores the value for the patients age, updates based on their DOB.',
+  \`dob\` date NOT NULL COMMENT 'Stores the date of birth for the patient',
+  \`test_results\` json DEFAULT NULL,
+  PRIMARY KEY (\`ID\`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='This table stores patient information';
+`
+pool.query(createPatientsTable);
+
 const valuesForUsers = ['email@host.com','example_pwrd',1];
 
 const updateResultsData = `
