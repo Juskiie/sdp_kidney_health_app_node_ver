53990de04a3a2643a026f7c436d86267297b5159
Juskiie <juskiehusky@icloud.com>
2023-03-15 15:26:52 +0000

    Refactored new dashboards. Fixed code so it updates efgr on live page

--- public/scripts/submitForm.js
+++ public/scripts/submitForm.js
@@ -20,12 +20,43 @@ function submitForm(event) {
 		console.log(efgrResult);
 		alert("Your EFGR value is: "+efgrResult);
 
+		let resultMessageElement = document.querySelector('#result-message');
+		const rightBoxElement = document.querySelector('.results');
+
+		switch(true) {
+			case efgrResult >= 90:
+				rightBoxElement.style.backgroundColor = '#C9CC3F';
+				resultMessageElement.innerHTML = 'This is considered normal.';
+				break;
+			case efgrResult >= 60 && efgrResult <= 89:
+				rightBoxElement.style.backgroundColor = '#E7DFC9';
+				resultMessageElement.innerHTML = 'This is considered mildly low.';
+				break;
+			case efgrResult >= 45 && efgrResult <= 59:
+				rightBoxElement.style.backgroundColor = '#E8D5CE';
+				resultMessageElement.innerHTML = 'This is considered moderately low.';
+				break;
+			case efgrResult >= 30 && efgrResult <= 44:
+				rightBoxElement.style.backgroundColor = '#E8CDD2';
+				resultMessageElement.innerHTML = 'This is considered severely low.';
+				break;
+			case efgrResult >= 15 && efgrResult <= 29:
+				rightBoxElement.style.backgroundColor = '#D4C3CA';
+				resultMessageElement.innerHTML = 'This is considered very severely low.';
+				break;
+			default:
+				rightBoxElement.style.backgroundColor = '#D4C3CA';
+				resultMessageElement.innerHTML = 'Your eGFR result is extremely low. Please see your doctor immediately.';
+				break;
+		}
+
 		return false;
 	} catch (e) {
 		console.error(e);
 	} finally {
 		return false;
 	}
+
 }
 
 document.querySelector('#testForm').addEventListener('submit', submitForm);
