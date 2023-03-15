fc4327bd132650d204e1b9cc7b6085931b442be0
Juskiie <juskiehusky@icloud.com>
2023-03-15 16:06:38 +0000

    Merge remote-tracking branch 'origin/master'

--- public/scripts/submitForm.js
+++ public/scripts/submitForm.js
@@ -3,17 +3,17 @@ console.log("submitForm.js loaded");
 function submitForm(event) {
 	try{
 		event.preventDefault();
-		let gender = document.querySelector('select[name="gender"]').value;
+		let sex = document.querySelector('select[name="gender"]').value;
 		let age = document.querySelector('input[name="age"]').value;
 		let ethnicity = document.querySelector('select[name="ethnicity"]').value;
 		let creatinine = document.querySelector('input[name="creat"]').value;
-		let isFemale = gender==="female";
+		let isFemale = sex==="female";
 		let isBlack = ethnicity==="black";
 		const efgrResult = calcEFGR(creatinine, age, isFemale, isBlack);
 		document.querySelector('.result').textContent = efgrResult;
 
 		// Do something with the form values here
-		console.log("Gender:", gender);
+		console.log("Sex:", sex);
 		console.log("Age:", age);
 		console.log("Ethnicity:", ethnicity);
 		console.log("Creatinine:", creatinine);
