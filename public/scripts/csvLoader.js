dfbcaac65aac6468cfa70c8b417004274fbba78c
Juskiie <juskiehusky@icloud.com>
2023-03-29 13:16:53 +0100

    - Added CSV upload for clinicians

---
+++ public/scripts/csvLoader.js
@@ -0,0 +1,54 @@
+console.log("csvLoader.js loaded");
+import calcEFGR from './calcEFGR.js';
+
+export function csvLoader(csvData) {
+	const lines = csvData.split('\n');
+	const patients = [];
+	const patientIds = new Set();
+
+	for (const line of lines) {
+		const values = line.split(',');
+
+		if (values.length !== 5) {
+			continue; // Skip invalid lines
+		}
+
+		const [patientID, rawGender, ethnicity, age, creatinine] = values;
+		const gender = rawGender === '1' ? 'Male' : 'Female';
+
+		// Validate age and creatinine
+		const ageInt = parseInt(age, 10);
+		const creatinineInt = parseInt(creatinine, 10);
+
+		if (ageInt < 0 || ageInt > 105 || creatinineInt < 0 || isNaN(ageInt) || isNaN(creatinineInt)) {
+			continue;
+		}
+
+		// Skip duplicate patient data, only one per patient can exist in the CSV.
+		if (patientIds.has(patientID)) {
+			console.log(`Duplicate PatientID: ${patientID}`);
+			continue;
+		}
+
+		patientIds.add(patientID);
+		patients.push({
+			patientID,
+			gender,
+			ethnicity,
+			age: ageInt,
+			creatinine: creatinineInt,
+		});
+	}
+
+	for (const patient of patients) {
+		console.log(calcEFGR(patient.creatinine, patient.age, patient.gender, patient.ethnicity));
+	}
+}
+
+document.getElementById('csvFile').addEventListener('change', async (event) => {
+	const file = event.target.files[0];
+	if (file) {
+		const fileContent = await file.text();
+		csvLoader(fileContent);
+	}
+});
